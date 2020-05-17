f736d5d216899901f1acbab9bb7caec5a8589c94
mdrdani <sudokill3004@gmail.com>
2020-05-17 13:50:12 +0700

    test phpunit API themoviedb

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -10,12 +10,6 @@
             <span class="mx-2">|</span>
             <span>{{\Carbon\Carbon::parse($movie['release_date'])->format('d M, Y')}}</span>
         </div>
-        <div class="text-gray-400 text-sm">
-            @foreach ($movie['genre_ids'] as $genre)
-                {{  $genres->get($genre) }} @if (!$loop->last)
-                    ,
-                @endif
-            @endforeach
-        </div>
+        <div class="text-gray-400 text-sm">@foreach ($movie['genre_ids'] as $genre){{  $genres->get($genre) }}@if (!$loop->last),@endif @endforeach</div>
     </div>
 </div>
