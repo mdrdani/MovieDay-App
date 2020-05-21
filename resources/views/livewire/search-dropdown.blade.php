a82eb8d1eab70a055bf6aa8a23bb0ff6c4641780
mdrdani <sudokill3004@gmail.com>
2020-05-21 12:09:04 +0700

    UI Interactive with Alpine.js

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,17 +1,35 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
-    <div class="relative mt-3 md:mt-0">
-        <input wire:model.debounce.500ms="search" type="text" class="bg-white rounded-full text-black text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+        <input wire:model.debounce.500ms="search" type="text" class="bg-white rounded-full text-black text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" 
+        placeholder="Search"
+        x-ref="search"
+        @keydown.window="
+            if (event.keyCode === 191) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
+        @focus="isOpen = true"
+        @keydown="isOpen = true"
+        @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false"
+        >
         <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
-    </div>
+    
 
     @if (strlen($search) > 2)    
-        <div class="absolute bg-gray-800 text-sm rounded w-64 mt-4">
+        <div 
+        class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4" 
+        x-show,transition.opactiy="isOpen"
+        @keydown.escape.window="isOpen = false"
+        >
             @if ($searchResults->count() > 0)
             <ul>
                 @foreach ($searchResults as $result)
                     <li class="border-b border-gray-700">
-                        <a href="{{route('movies.show', $result['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                        <a 
+                        href="{{route('movies.show', $result['id'])}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center"
+                        @if($loop->last) @keydown.tab="isOpen = false" @endif
+                        >
                             @if ($result['poster_path'])
                                 <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path']}}" alt="poster" class="w-8">
                             @else
